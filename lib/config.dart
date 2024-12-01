8df5d264e1c054887434701776ee2eb6de88091b
mraccah2 <mraccah@gmail.com>
2024-12-02 00:54:39 +0100

    added flag in config.dart to switch from proxy to direct API calls

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,9 @@
 class Config {
-  // Replace with your actual Google Maps API key
-  static const String googleMapsApiKey = 'AIzaSyACv9RKhgt9rhe-rdVhEaxniJAEyejOE1E';
+  static const bool useLocalProxy = false;
+
+  static const String googleMapsApiKey = 'YOUR_GOOGLE_MAPS_API_KEY';
+
+  static String get baseUrl {
+    return useLocalProxy ? 'http://localhost:8080' : 'https://maps.googleapis.com/maps/api';
+  }
 } 
