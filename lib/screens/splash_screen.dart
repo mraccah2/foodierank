daca026a4a6a0c8d13118f7d543e52f72552dafd
mraccah2 <mraccah@gmail.com>
2024-12-03 21:23:10 +0100

    Add error reporting in case restaurant load doesn't work.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -11,6 +11,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   bool _dataLoaded = false;
+  String? _error;
 
   @override
   void initState() {
@@ -20,18 +21,69 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _loadData() async {
-    // Start loading restaurants and caching photos
-    await RestaurantService.instance.loadAndCacheRestaurants();
-    setState(() {
-      _dataLoaded = true;
-    });
+    try {
+      await RestaurantService.instance.loadAndCacheRestaurants();
+      setState(() {
+        _dataLoaded = true;
+      });
+    } catch (e, stackTrace) {
+      setState(() {
+        _error = '''
+Error loading restaurant data:
+$e
+
+Technical details:
+$stackTrace
+''';
+      });
+    }
   }
 
   void _navigateToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
     if (_dataLoaded) {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const RestaurantListScreen()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              title: const Text('Error Loading Data'),
+            ),
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Unable to load restaurant data',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SelectableText(_error ?? 'Timeout while loading restaurant data'),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => const SplashScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text('Retry'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       );
     }
   }
