9936151c84d1dea58036933d608fac2742a31c62
mraccah2 <mraccah@gmail.com>
2024-12-08 22:22:46 +0100

    Added package name and sha1 to headers to allow calling from android. Build now working on android.

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,21 @@
+import 'dart:io' show Platform;
+
 class Config {
   static const bool useLocalProxy = false;
 
-  static const String googleMapsApiKey = 'AIzaSyBl5mwMAHNMmDAq1now6jsnQzZAgxGm31s';
+  // Platform-specific API keys
+  static const String _androidApiKey = 'AIzaSyBjwQIiRllZzygrwLJSluNJ__C_sRZGb8I';
+  static const String _iosApiKey = 'AIzaSyBl5mwMAHNMmDAq1now6jsnQzZAgxGm31s';
+
+  // Get the appropriate API key based on platform
+  static String get googleMapsApiKey {
+    if (Platform.isAndroid) {
+      return _androidApiKey;
+    } else if (Platform.isIOS) {
+      return _iosApiKey;
+    }
+    throw UnsupportedError('Unsupported platform for Google Maps API');
+  }
 
   static String get baseUrl {
     return useLocalProxy ? 'http://localhost:8080' : 'https://maps.googleapis.com/maps/api';
